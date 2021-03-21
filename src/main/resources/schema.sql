445148c53eff03e113b431dc22d43b8829b2b418
Ben Hession <bshession@gmail.com>
2021-03-21 10:52:32 +0000

    Make appropriate fields not null.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,31 +9,31 @@ create table student (
 );
 
 create table "module" (
-    module_code varchar(5),
-    module_year varchar(4),
-    module_name varchar(50),
+    module_code varchar(5) not null,
+    module_year varchar(4) not null,
+    module_name varchar(50) not null,
 
     primary key(module_code, module_year)
 );
 
 create table tutor (
-    tutor_id varchar(13),
-    forename varchar(20),
-    surname varchar(20),
+    tutor_id varchar(13) not null,
+    forename varchar(20) not null,
+    surname varchar(20) not null,
 
     primary key (tutor_id)
 );
 
 create table class (
-    class_id varchar(12),
-    class_name varchar(50),
-    location varchar(50),
-    date_time timestamp,
-    duration interval,
-    class_type varchar(15),
-    qr_string varchar(50),
-    module_code varchar(5),
-    module_year varchar(4),
+    class_id varchar(12) not null,
+    class_name varchar(50) not null,
+    location varchar(50) not null,
+    date_time timestamp not null,
+    duration interval not null,
+    class_type varchar(15) not null,
+    qr_string varchar(32) not null,
+    module_code varchar(5) not null,
+    module_year varchar(4) not null,
     tutor_id varchar(13),
 
     primary key (class_id)
@@ -43,9 +43,9 @@ alter table class add foreign key (module_code, module_year) references "module"
 alter table class add foreign key (tutor_id) references tutor(tutor_id);
 
 create table student_class (
-    student_id varchar(12),
-    class_id varchar(12),
-    attended boolean,
+    student_id varchar(12) not null,
+    class_id varchar(12) not null,
+    attended boolean not null,
 
     primary key (student_id, class_id)
 );
