c3784bd851e1a9846d88ec37b912c6aa34604fa3
Ben Hession <bshession@gmail.com>
2021-04-07 22:30:27 +0100

    Add an endpoint to return the university classes of a logged in user.

--- src/main/java/com/benhession/attendance_web_service/controllers/StudentController.java
+++ src/main/java/com/benhession/attendance_web_service/controllers/StudentController.java
@@ -2,11 +2,17 @@ package com.benhession.attendance_web_service.controllers;
 
 import com.benhession.attendance_web_service.data.StudentService;
 import com.benhession.attendance_web_service.model.Student;
+import com.benhession.attendance_web_service.model.StudentUniversityClass;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 import java.util.Set;
 
 
@@ -25,6 +31,24 @@ public class StudentController {
     public Set<Student> findAllStudents() {
 
         return studentService.findAllStudents();
+    }
+
+    @GetMapping("/classes")
+    public ResponseEntity<Set<StudentUniversityClass>> getClassesOfStudent(@AuthenticationPrincipal Jwt principal) {
+
+        String studentId = principal.getClaim("user_name");
+
+        System.out.println(studentId);
+
+        Optional<Set<StudentUniversityClass>> studentClasses =
+                Optional.ofNullable(studentService.universityClassesOfRequester(studentId));
+
+        return studentClasses.map(
+                studentUniversityClasses -> new ResponseEntity<>(studentUniversityClasses, HttpStatus.OK)
+        ).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NO_CONTENT));
+
+
+
     }
 
 
