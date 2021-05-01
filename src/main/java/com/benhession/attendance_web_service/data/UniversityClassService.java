0e80450226ce76d630e041a68b96b484e52cf13c
Ben Hession <bshession@gmail.com>
2021-05-01 17:47:49 +0100

    Add findAll and saveAll methods for testing purposes.

--- src/main/java/com/benhession/attendance_web_service/data/UniversityClassService.java
+++ src/main/java/com/benhession/attendance_web_service/data/UniversityClassService.java
@@ -4,7 +4,9 @@ import com.benhession.attendance_web_service.model.UniversityClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UniversityClassService {
@@ -20,4 +22,26 @@ public class UniversityClassService {
 
         return classRepository.findById(classId);
     }
+
+    // util method for testing
+    public Set<UniversityClass> findAll() {
+
+        Set<UniversityClass> universityClasses = new HashSet<>();
+        Iterable<UniversityClass> classIterable = classRepository.findAll();
+
+        classIterable.forEach(universityClasses::add);
+
+        return universityClasses;
+    }
+
+    // util method for testing
+    public boolean saveAll(Set<UniversityClass> classSet) {
+        Iterable<UniversityClass> classIterable = classRepository.saveAll(classSet);
+        Set<UniversityClass> newClassSet = new HashSet<>();
+
+        classIterable.forEach(newClassSet::add);
+
+        return newClassSet.equals(classSet);
+    }
+
 }
